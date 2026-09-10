cb7843959f529efe4da7f00954ec5408bd57e686
Elias58gf <elias58gf@gmail.com>
2026-09-10 00:34:14 +0000

    feat: adicionar testes para a função de conversão de taxa anual para mensal

---
+++ tests/tests/test_conversao_taxa.py
@@ -0,0 +1,19 @@
+import pytest
+from fincalc import converter_taxa_anual_para_mensal
+
+def test_conversao_taxa_positiva():
+    # Arrange & Act
+    taxa_m = converter_taxa_anual_para_mensal(12.6825)
+    # Assert
+    assert round(taxa_m, 4) == 1.0000
+
+def test_conversao_taxa_nula():
+    # Arrange & Act
+    taxa_m = converter_taxa_anual_para_mensal(0.0)
+    # Assert
+    assert round(taxa_m, 4) == 0.0
+
+def test_conversao_taxa_abaixo_menos_cem():
+    # Arrange, Act & Assert
+    with pytest.raises(ValueError):
+        converter_taxa_anual_para_mensal(-150.0)
