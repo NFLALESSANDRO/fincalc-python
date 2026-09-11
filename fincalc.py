405892540aa928cfe4fbc68e6cae35005c8881a6
AlberthAMR <125111385579@ulife.com.br>
2026-09-11 18:39:07 -0300

    revert: remove validacao da conversao de taxa, que cabe ao responsavel pela funcao

--- fincalc.py
+++ fincalc.py
@@ -72,8 +72,7 @@ def calcular_rendimento_real(ganho_nominal: float, inflacao: float) -> float:
 
 def converter_taxa_anual_para_mensal(taxa_anual: float) -> float:
     """Converte uma taxa de juros anual equivalente para taxa mensal."""
-    if taxa_anual < -100:
-        raise ValueError("Taxa anual não pode ser menor que -100%.")
+
     return (((1 + (taxa_anual / 100)) ** (1 / 12)) - 1) * 100
 
 
